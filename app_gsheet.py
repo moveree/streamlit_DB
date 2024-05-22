a59071eff4d51968fee98c814a2c766befbdf84c
hanae <146168847+moveree@users.noreply.github.com>
2024-05-22 16:25:47 +0200

    Update app_gsheet.py

--- app_gsheet.py
+++ app_gsheet.py
@@ -3,7 +3,7 @@ import streamlit as st
 from streamlit_gsheets import GSheetsConnection
 # Create a connection object.
 url = "https://docs.google.com/spreadsheets/d/1L19AF2t_svkYHtRhAgxg0vLliIklD4wKlK3AwcgiN3o/edit#gid=0"
-conn = st.connection("gsheets", type=GSheetsConnection)
+conn = st.experimental_connection("gsheets", type=GSheetsConnection)
 
 df = conn.read(
     spreadsheet = url,
